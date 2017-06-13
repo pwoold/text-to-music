5a03c2cd6723e29fac02b497830c7516e89afa92
Phoebe Wooldridge <pwoold@gmail.com>
2017-06-13 13:07:36 -0500

    only 30 different values available for each ch

--- string_conversion.py
+++ string_conversion.py
@@ -2,6 +2,8 @@ def string_to_ascii(s):
 
     ascii_values = []
     for ch in s:
-        ascii_values.append(ord(ch))
+        ascii_values.append(ord(ch)%30)
         
     return ascii_values
+
+print(string_to_ascii("hello"))
